aaa85943e154435f2ced1d11edbe7818adf3ebb6
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-25 18:57:18 +0300

    переделка примерка под Spring-Kafka

--- src/main/java/ru/datana/kafka/gateway/DatanaKafkaConsumerApp.java
+++ src/main/java/ru/datana/kafka/gateway/DatanaKafkaConsumerApp.java
@@ -1,23 +1,12 @@
 package ru.datana.kafka.gateway;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.GenericMessageListener;
 import ru.datana.kafka.gateway.config.AppConts;
-import ru.datana.kafka.gateway.config.AppOptions;
-import ru.datana.kafka.gateway.listener.DatanaKafkaListener;
-import ru.datana.kafka.gateway.utils.AppException;
 
 import java.util.Arrays;
-import java.util.Properties;
-import java.util.UUID;
 
 @Slf4j
 @SpringBootApplication
@@ -28,7 +17,7 @@ public class DatanaKafkaConsumerApp {
         try {
             SpringApplication app = new SpringApplication(DatanaKafkaConsumerApp.class);
             app.setBannerMode(Banner.Mode.OFF);
-            createSystemConsumer();
+            ///createSystemConsumer();
             app.run(args);
         } catch (Exception ex) {
             log.error(AppConts.ERROR_LOG_PREFIX + " Ошибка в программе", ex);
@@ -36,22 +25,22 @@ public class DatanaKafkaConsumerApp {
         log.info(AppConts.APP_LOG_PREFIX + "********* Завершение программы *********");
     }
 
-    private static void createSystemConsumer() throws AppException {
-
-        AppOptions appOptions = new AppOptions();
-        appOptions.load();
-        String name = appOptions.getKafkaTopic();
-        log.info("Creating kafka consumer for topic {}", name);
-        ContainerProperties containerProps = new ContainerProperties(name);
-
-        Properties props = appOptions.getProperties();
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
-        ConsumerFactory<String, String> factory = new DefaultKafkaConsumerFactory(props);
-
-        ConcurrentMessageListenerContainer<String, GenericMessageListener> container =
-                new ConcurrentMessageListenerContainer(factory, containerProps);
-        container.setupMessageListener(new DatanaKafkaListener());
-        container.start();
-        log.info("Successfully created kafka consumer for topic {}", name);
-    }
+//    private static void createSystemConsumer() throws AppException {
+//
+//        AppOptions appOptions = new AppOptions();
+//        appOptions.load();
+//        String name = appOptions.getKafkaTopic();
+//        log.info("Creating kafka consumer for topic {}", name);
+//        ContainerProperties containerProps = new ContainerProperties(name);
+//
+//        Properties props = appOptions.getProperties();
+//        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+//        ConsumerFactory<String, String> factory = new DefaultKafkaConsumerFactory(props);
+//
+//        ConcurrentMessageListenerContainer<String, GenericMessageListener> container =
+//                new ConcurrentMessageListenerContainer(factory, containerProps);
+//        container.setupMessageListener(new DatanaKafkaListener());
+//        container.start();
+//        log.info("Successfully created kafka consumer for topic {}", name);
+//    }
 }
--- src/main/java/ru/datana/kafka/gateway/DatanaKafkaProducerApp.java
+++ src/main/java/ru/datana/kafka/gateway/DatanaKafkaProducerApp.java
@@ -1,8 +1,6 @@
 package ru.datana.kafka.gateway.producer;
 
 import lombok.extern.slf4j.Slf4j;
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.inf.ArgumentParser;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -11,7 +9,6 @@ import ru.datana.kafka.gateway.config.AppOptions;
 
 import java.util.Arrays;
 
-import static net.sourceforge.argparse4j.impl.Arguments.store;
 
 @Slf4j
 public class DatanaKafkaProducerApp {
@@ -46,50 +43,5 @@ public class DatanaKafkaProducerApp {
         log.info(AppConts.APP_LOG_PREFIX + "********* Завершение программы *********");
     }
 
-    /**
-     * Get the command-line argument parser.
-     */
-    private static ArgumentParser argParser() {
-        ArgumentParser parser = ArgumentParsers
-                .newArgumentParser("simple-producer")
-                .defaultHelp(true)
-                .description("This example is to demonstrate kafka producer capabilities");
-
-        parser.addArgument("--app.dir").action(store())
-                .required(true)
-                .type(String.class)
-                .metavar("APP-DIR")
-                .help("path to file "+APP_CONFIG_FILE_NAME);
-
-        parser.addArgument("--messages").action(store())
-                .required(true)
-                .type(Long.class)
-                .metavar("NUM-MESSAGE")
-                .help("number of messages to produce");
-
-        parser.addArgument("--syncsend").action(store())
-                .required(false)
-                .type(Boolean.class)
-                .setDefault(true)
-                .metavar("TRUE/FALSE")
-                .help("sync/async send of messages");
-
-        parser.addArgument("--delay").action(store())
-                .required(false)
-                .setDefault(10l)
-                .type(Long.class)
-                .metavar("DELAY")
-                .help("number of milli seconds delay between messages.");
-
-        parser.addArgument("--messagetype").action(store())
-                .required(false)
-                .setDefault("string")
-                .type(String.class)
-                .choices(Arrays.asList("string", "myevent"))
-                .metavar("STRING/MYEVENT")
-                .help("generate string messages or MyEvent messages");
-
-        return parser;
-    }
 
 }

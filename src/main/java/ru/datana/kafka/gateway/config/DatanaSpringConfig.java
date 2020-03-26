7f8f80cbab6e4237dd1be4eda4438735ddba2bb4
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-26 15:00:07 +0300

    переделка примерка под Spring-Kafka

--- src/main/java/ru/datana/kafka/gateway/config/DatanaSpringConfig.java
+++ src/main/java/ru/datana/kafka/gateway/config/DatanaSpringConfig.java
@@ -2,7 +2,6 @@ package ru.datana.kafka.gateway.config;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -10,7 +9,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.MessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import ru.datana.kafka.gateway.utils.AppException;
 
 import java.util.Properties;
@@ -21,10 +19,10 @@ public class DatanaSpringConfig {
 
     @Bean
     public ConsumerFactory<String, MessageListenerContainer> consumerFactory() throws AppException {
-        JsonDeserializer<MessageListenerContainer> deserializer = new JsonDeserializer<>(MessageListenerContainer.class);
-        deserializer.setRemoveTypeHeaders(false);
-        deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeMapperForKey(true);
+//        JsonDeserializer<MessageListenerContainer> deserializer = new JsonDeserializer<>(MessageListenerContainer.class);
+//        deserializer.setRemoveTypeHeaders(false);
+//        deserializer.addTrustedPackages("*");
+//        deserializer.setUseTypeMapperForKey(true);
 
 
         AppOptions appOptions = new AppOptions();
@@ -33,10 +31,10 @@ public class DatanaSpringConfig {
         properties.setProperty(CommonClientConfigs.CLIENT_ID_CONFIG, "datana-consumer");
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        //properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        //properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        return new DefaultKafkaConsumerFactory(properties, new StringDeserializer(), deserializer);
+        return new DefaultKafkaConsumerFactory(properties/*, new StringDeserializer(), deserializer */);
     }
 
     @Bean
